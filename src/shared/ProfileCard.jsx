e9387bb7fe0170ff0d393018468ffcdb8e0f587f
nurudeen99 <nurudeen99@gmail.com>
2024-01-30 10:20:52 +0100

    added a greeting function to te profile card

--- src/shared/ProfileCard.jsx
+++ src/shared/ProfileCard.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react"
 
 const ProfileCard = ({image,age,birthDate,bloodGroup,email,eyeColor,firstName,lastName,gender,height,university,phone,weight,maidenName,userAgent,company,address}) => {
+     const today = new Date()
+    //  console.log(today)
+     const now = today.getHours()
+    //  const [time,setTime] = useState(0)
+
+     function todayTime(){
+      if(now <= 12 ){
+        return(
+          "Good Morning"
+        )
+        }else if(now <= 16 ){
+          return (
+            "Good Aternoon"
+          )
+        }else{
+          return(
+            "Good Evening"
+          )
+        }
+     }
+
   return (
 
     <div className="flex justify-center items-center w-full  p-[3rem] min-h-screen top-0 fixed">
         <div className=" font-bold bg-white flex flex-col item-center rounded-[0.5rem] h-screen  gap-y-[0.5rem] items-center drop-shadow-xl rounded-[1.5rem] text-center w-[40rem] p-[1rem]">
             <div className="text-4xl">
-                <h1><span className="text-green-500">WELCOME</span> <span className="text-blue-500">{firstName}</span> <span className="text-yellow-500">{lastName}</span></h1>
+                <h1><span className="text-green-500">{todayTime()}</span> <span className="text-blue-500">{firstName}</span> <span className="text-yellow-500">{lastName}</span></h1>
             </div>
             <div className="w-[6rem]"><img src={image} alt="image" /></div>
                        <p>FULL NAME: {firstName} {lastName}</p>
